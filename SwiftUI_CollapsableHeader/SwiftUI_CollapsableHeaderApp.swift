39a73b910323243e5ab309498ce95fb6252d4611
boardguy1024 <boardguy1024@gmail.com>
2022-11-07 15:19:46 +0900

    Initial Commit

---
+++ SwiftUI_CollapsableHeader/SwiftUI_CollapsableHeaderApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_CollapsableHeaderApp.swift
+//  SwiftUI_CollapsableHeader
+//
+//  Created by park kyung seok on 2022/11/07.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_CollapsableHeaderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
